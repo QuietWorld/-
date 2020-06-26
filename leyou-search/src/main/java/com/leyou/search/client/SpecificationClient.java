8e685e185b57f9c3d6875e66d279c7b73c937171
zhangchun <1174071683@qq.com>
2020-06-27 01:44:23 +0800

    新增了使用ElasticSearch实现全文检索的leyou-search模块，并对以往的微服务方法和注释进行了规范和完善。

--- leyou-search/src/main/java/com/leyou/search/client/SpecificationClient.java
+++ leyou-search/src/main/java/com/leyou/search/client/SpecificationClient.java
@@ -1,15 +1,11 @@
 package com.leyou.search.client;
 
-import com.leyou.item.domain.SpecGroup;
+import com.leyou.item.api.SpecificationApi;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
+/**
+ * @author zc
+ */
 @FeignClient("item-service")
-public interface SpecificationClient {
-
-    @GetMapping("/spec/groups/{cid}")
-    List<SpecGroup> listSpecGroups(@PathVariable("cid")Long cid);
+public interface SpecificationClient extends SpecificationApi {
 }
